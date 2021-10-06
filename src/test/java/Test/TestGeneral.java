890a38e7d50b88cf47c4cfc5ebc240ca2cc48aa9
CatatumboGirl <82122272+CatatumboGirl@users.noreply.github.com>
2021-10-05 23:39:48 -0500

    Update TestGeneral, Add test verificarGuardarNuevaEmpresa

--- src/test/java/Test/TestGeneral.java
+++ src/test/java/Test/TestGeneral.java
@@ -1,10 +1,12 @@
 package Test;
 
 import java.sql.SQLException;
+import logica.Empresa;
 import logica.LogicaEmpresa;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import persistencia.ConexionBD;
+import persistencia.EmpresaDAO;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -23,7 +25,22 @@ public class TestGeneral {
         Assert.assertTrue(con.isConectado(), "No hay conexión creada. ");
         con.desconectar();
         Assert.assertFalse(con.isConectado(), "No se cerró conexión. ");
-    }}
+    }
+   @Test
+    public void verificarGuardarNuevaEmpresa() {
+        EmpresaDAO dao = new EmpresaDAO();
+        Empresa e = new Empresa ("Empresa de prueba","4234224","Maximiliano","877887",1,2393,0,"Santander","Barrancabermeja");
+        int id = dao.guardarNuevaEmpresa(e);
+        Assert.assertTrue(id > 0, "La empresa no fue guardada");
+        String sql = "DELETE FROM empresa WHERE id_empresa = " + id;
+        ConexionBD con = new ConexionBD();
+        int cant = con.ejecutarUpdate(sql);
+        Assert.assertTrue(cant == 1, "El dato de prueba no pudo ser eliminado ");
+        con.desconectar();
+    }
+
+
+}
     
     
     
